fed87a2218a7560389054b6746082c77eae4dc04
JAYESH VASANI <jayesh.vasani1979@marwadieducation.edu.in>
2021-12-22 12:11:18 +0530

    Pull

---
+++ circket/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:circket/homepage.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(appBarTheme: AppBarTheme(color: Colors.blue[700])),
+      home: Scaffold(
+        body: HomePage(),
+      ),
+    );
+  }
+}
